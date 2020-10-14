b12ed256a033d27616ac1bc927fd3ccdd8658b5e
urancon <ulysse.rancon@gmail.com>
2020-10-14 21:52:21 +0200

    change dataset format to be compatible with NVlab's SPADE

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -4,16 +4,16 @@
 # Input: original CAMUS dataset hdf5 file
 # Output: three separate hdf5 files ('train', 'test', 'valid') with 1-channel images
 
+import time
 import os
 import sys
 import h5py
 import numpy as np
-
+from tqdm import tqdm
 
 print('\n')
 camus_path = "/home/ulysse/Desktop/ProjetGAN/Data/camus05_256.hdf5"
 f = h5py.File(camus_path, 'r')
-
 
 for set in ['train', 'test', 'valid']:
 
@@ -23,10 +23,9 @@ for set in ['train', 'test', 'valid']:
 
     with h5py.File(new_dset, 'w') as newdatafile:
         with h5py.File("anno_" + new_dset, 'w') as newannofile:
-
-            for patient in f[set]:
-
-                image = f[set][patient]['4CH']['im'][0, :, :, :]  # image du patient - 4 chambres - fin diastole (canal 0 seulement)
+            for patient in tqdm(f[set]):
+                image = f[set][patient]['4CH']['im'][0, :, :,
+                        :]  # image du patient - 4 chambres - fin diastole (canal 0 seulement)
                 mask = f[set][patient]['4CH']['gt'][0, :, :]  # masque correspondant
                 newdatafile.create_dataset(patient, data=image)
                 newannofile.create_dataset(patient, data=mask)
@@ -40,4 +39,3 @@ for set in ['train', 'test', 'valid']:
         newdatafile.close()
 
 f.close()
-
